8f2e2d7f6bfebd55e8783915b65f2679ad480f02
ndifix <yoshiki.linuxmint@gmail.com>
2024-05-30 21:52:47 +0900

    add のテストを追加

--- src/arithmetic/test.rs
+++ src/arithmetic/test.rs
@@ -13,3 +13,37 @@ fn fromint_negative_one_digit() {
     let str = bi.to_string();
     assert_eq!(str, "-37");
 }
+
+#[test]
+fn add_psmall_psmall() {
+    let lhs = Bigint::from_int(100_000);
+    let rhs = Bigint::from_int(200_000);
+    let result = lhs + rhs;
+    assert_eq!(result.to_string(), "300000");
+}
+
+#[test]
+#[should_panic]
+fn add_nsmall_psmall() {
+    let lhs = Bigint::from_int(-100_000);
+    let rhs = Bigint::from_int(200_000);
+    let result = lhs + rhs;
+    assert_eq!(result.to_string(), "300000");
+}
+
+#[test]
+#[should_panic]
+fn add_psmall_nsmall() {
+    let lhs = Bigint::from_int(100_000);
+    let rhs = Bigint::from_int(-200_000);
+    let result = lhs + rhs;
+    assert_eq!(result.to_string(), "300000");
+}
+
+#[test]
+fn add_nsmall_nsmall() {
+    let lhs = Bigint::from_int(-100_000);
+    let rhs = Bigint::from_int(-200_000);
+    let result = lhs + rhs;
+    assert_eq!(result.to_string(), "-300000");
+}
